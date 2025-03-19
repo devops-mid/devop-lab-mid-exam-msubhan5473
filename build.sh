23946d5d1db679b9e394be69fa247ea61e38d9d8
Ammar-Kurt <ammar3172003@gmail.com>
2025-03-19 09:29:57 +0500

    Fixed build.sh script

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "Building the application..."
 # TODO: Add commands to install dependencies and build the app
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Running unit tests..."
+pytest
